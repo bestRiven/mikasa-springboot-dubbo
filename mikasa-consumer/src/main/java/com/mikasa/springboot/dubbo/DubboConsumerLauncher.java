f0670a9b80709cf102b6aa1c0ad81c5ddfe00ede
admin <13051476753@163.com>
2017-03-25 22:51:48 +0800

    完善项目

--- mikasa-consumer/src/main/java/com/mikasa/springboot/dubbo/DubboConsumerLauncher.java
+++ mikasa-consumer/src/main/java/com/mikasa/springboot/dubbo/DubboConsumerLauncher.java
@@ -3,6 +3,8 @@ package com.mikasa.springboot.dubbo;
 import com.alibaba.boot.dubbo.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by zhangyu51 on 2017/3/24.
@@ -10,9 +12,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDubboConfiguration
-public class DubboConsumerLauncher {
+public class DubboConsumerLauncher extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DubboConsumerLauncher.class, args);
+    }
+
+    /**
+     * 如果要发布到自己的Tomcat中的时候，需要继承SpringBootServletInitializer类，并且增加如下的configure方法。
+     * 如果不发布到自己的Tomcat中的时候，就无需上述的步骤
+     */
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(DubboConsumerLauncher.class);
     }
 }
